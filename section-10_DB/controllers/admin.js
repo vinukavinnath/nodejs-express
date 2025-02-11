803c17f3767fdf572494178830a5138cb5fb3679
Vinuka Vinnath <vinnathwlv@gmail.com>
2025-02-11 21:57:57 +0530

    Section 11: Fetch product details by product Id implemented

--- section-10_DB/controllers/admin.js
+++ section-10_DB/controllers/admin.js
@@ -3,13 +3,18 @@ const { v4: uuidv4 } = require('uuid');
 
 exports.getAdminPage = (req, res) => {
 
-    Product.fetchAll().then(([rows, fieldData]) =>
-        res.render('admin/admin',
-            { products: rows, pageTitle: 'All Products', isAdmin: true })
-    ).catch(err => {
-        console.log(err);
-    });
+    Product.findAll()
+        .then(products => {
+            console.log("--------------Logging Products----------------------");
 
+            // In products array there is Product>dataValues
+            // But sequelize offers dataValues at Top Level
+            // No need to access product details by <h2><%= product.Product.dataValues.id %></h2>
+            console.log(products);
+            res.render('admin/admin',
+                { products: products, isAdmin: true, pageTitle: 'All Products' });
+        })
+        .catch(err => console.log(err));
 }
 
 exports.getAddProductPage = (req, res) => {
--- section-10_DB/controllers/products.js
+++ section-10_DB/controllers/products.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const Product = require('../models/products');
 
 
@@ -7,6 +8,9 @@ exports.getAllProducts = (req, res) => {
         .then(products => {
             console.log("--------------Logging Products----------------------");
 
+            // In products array there is Product>dataValues
+            // But sequelize offers dataValues at Top Level
+            // No need to access product details by <h2><%= product.Product.dataValues.id %></h2>
             console.log(products);
             res.render('shop/all-products',
                 { products: products, isAdmin: false, pageTitle: 'All Products' });
@@ -17,16 +21,36 @@ exports.getAllProducts = (req, res) => {
 exports.getProductById = (req, res) => {
     const productId = req.params.productId;
 
-    Product.findById(productId)
-        .then(([productData, fieldData]) => {
-            // console.log(result);
+    // Using findAll()
+    Product.findAll(
+        {
+            where: {
+                id: productId
+            }
+        }
+        
+    // Returns array of products even if there is only one product matches the query
+    ).then(([product]) => {
+        console.log(product);
 
-            res.render('shop/product-details', {
-                product: productData[0],
-                pageTitle: productData[0].title
-            });
-        })
+        res.render('shop/product-details', {
+            product: product,
+            pageTitle: product.title
+        });
+    })
         .catch(err => { console.log(err) });
+
+    // Using findByPk()
+    // Product.findByPk(productId)
+    //     .then((product) => {
+    //         console.log(product);
+
+    //         res.render('shop/product-details', {
+    //             product: product,
+    //             pageTitle: product.title
+    //         });
+    //     })
+    //     .catch(err => { console.log(err) });
 }
 
 exports.deleteProductById = (req, res) => {
